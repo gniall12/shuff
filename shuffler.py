145209ab16c30016268ab95e8de4ad201f17ab80
Niall Gallagher <niall.gallagher@ucdconnect.ie>
2019-11-17 17:28:13 +0000

    Modified functions to get playlist tracks and add tracks. API limits 100 tracks per request, so had to allow for multiple requests for playlists with more than 100

--- shuffler.py
+++ shuffler.py
@@ -5,31 +5,48 @@ from flask import abort
 
 base_url = config.base_url
 
+
 def shuffle(playlist, user_id, access_token):
     playlist_id = playlist['id']
     playlist_name = playlist['name']
-    headers={'Authorization': 'Bearer ' + access_token}
+    headers = {'Authorization': 'Bearer ' + access_token}
     old_tracks = get_playlist_tracks(playlist_id, headers)
     old_artist_ids = get_old_artists_ids(old_tracks)
     new_track_ids = get_new_track_ids(old_artist_ids, old_tracks, headers)
-    new_playlist = create_new_playlist(playlist_name, user_id, new_track_ids, headers)
+    new_playlist = create_new_playlist(
+        playlist_name, user_id, new_track_ids, headers)
     new_playlist_id = new_playlist['id']
     add_tracks_to_playlist(new_playlist_id, new_track_ids, headers)
     return new_playlist
 
+
 def get_playlist_tracks(playlist_id, headers):
     print('Getting playlist tracks...')
-    url = f'{base_url}playlists/{playlist_id}/tracks'
-    r = requests.get(url, headers=headers)
-    tracks = r.json()['items']
+    offset = 0
+    num_tracks_remaining = None
+    tracks = []
+    # Can only get 100 tracks per request - multiple requests may be needed
+    while True:
+        params = {'offset': offset}
+        url = f'{base_url}playlists/{playlist_id}/tracks'
+        r = requests.get(url, headers=headers, params=params)
+        if not num_tracks_remaining:
+            num_tracks_remaining = r.json()['total']
+        tracks.extend(r.json()['items'])
+        num_tracks_remaining -= 100
+        if num_tracks_remaining <= 0:
+            break
+        offset += 100
     return [track['track'] for track in tracks]
+
 
 def get_old_artists_ids(tracks):
     print('Getting old artist ids...')
     artist_ids = [track['artists'][0]['id'] for track in tracks]
     # Some artists have no ID - remove Nones from list
-    artist_ids_nones_removed = list(filter(None, artist_ids)) 
+    artist_ids_nones_removed = list(filter(None, artist_ids))
     return artist_ids_nones_removed
+
 
 def get_new_track_ids(old_artist_ids, old_tracks, headers):
     print('Getting new track ids...')
@@ -38,12 +55,13 @@ def get_new_track_ids(old_artist_ids, old_tracks, headers):
         track_added = False
         i = 0
         artist_top_tracks = get_artist_top_tracks(artist_id, headers)
-        while not track_added and i<len(artist_top_tracks):
+        while not track_added and i < len(artist_top_tracks):
             if not track_exists(artist_top_tracks[i], old_tracks + new_tracks):
                 new_tracks.append(artist_top_tracks[i])
                 track_added = True
             i += 1
     return [track['uri'] for track in new_tracks]
+
 
 def get_artist_top_tracks(artist_id, headers):
     url = f'{base_url}artists/{artist_id}/top-tracks?country=IE'
@@ -51,11 +69,13 @@ def get_artist_top_tracks(artist_id, headers):
     tracks = r.json()['tracks']
     return tracks
 
+
 def track_exists(track, existing_tracks):
     for existing_track in existing_tracks:
         if track['id'] == existing_track['id'] or track['name'] == existing_track['name']:
             return True
     return False
+
 
 def create_new_playlist(playlist_name, user_id, track_ids, headers):
     headers['Content-Type'] = 'application/json'
@@ -64,8 +84,13 @@ def create_new_playlist(playlist_name, user_id, track_ids, headers):
     r = requests.post(url, data=body, headers=headers)
     return r.json()
 
+
 def add_tracks_to_playlist(playlist_id, tracks, headers):
     url = f'{base_url}playlists/{playlist_id}/tracks'
-    body = json.dumps({'uris': tracks})
-    r = requests.post(url, data=body, headers=headers)
+    n = 100
+    tracks_split = [tracks[i:i+n] for i in range(0, len(tracks), n)]
+    # Can only add 100 tracks per request - multiple requests may be needed
+    for tracks_chunk in tracks_split:
+        body = json.dumps({'uris': tracks_chunk})
+        r = requests.post(url, data=body, headers=headers)
     return r.text
