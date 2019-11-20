18caeade535e1bf29713b128e16dc8d768cae08d
Niall Gallagher <niall.gallagher@ucdconnect.ie>
2019-11-20 21:14:26 +0000

    Extracted spotify api calls to separate module

--- shuffler.py
+++ shuffler.py
@@ -1,9 +1,5 @@
-import requests
-import json
-import config
-from flask import abort
-
-base_url = config.base_url
+from collections import Counter
+from spotify import get_playlist_tracks, get_artist_top_tracks, create_new_playlist, add_tracks_to_playlist
 
 
 def shuffle(playlist, user_id, access_token):
@@ -18,26 +14,6 @@ def shuffle(playlist, user_id, access_token):
     new_playlist_id = new_playlist['id']
     add_tracks_to_playlist(new_playlist_id, new_track_ids, headers)
     return new_playlist
-
-
-def get_playlist_tracks(playlist_id, headers):
-    print('Getting playlist tracks...')
-    offset = 0
-    num_tracks_remaining = None
-    tracks = []
-    # Can only get 100 tracks per request - multiple requests may be needed
-    while True:
-        params = {'offset': offset}
-        url = f'{base_url}playlists/{playlist_id}/tracks'
-        r = requests.get(url, headers=headers, params=params)
-        if not num_tracks_remaining:
-            num_tracks_remaining = r.json()['total']
-        tracks.extend(r.json()['items'])
-        num_tracks_remaining -= 100
-        if num_tracks_remaining <= 0:
-            break
-        offset += 100
-    return [track['track'] for track in tracks]
 
 
 def get_old_artists_ids(tracks):
@@ -63,34 +39,8 @@ def get_new_track_ids(old_artist_ids, old_tracks, headers):
     return [track['uri'] for track in new_tracks]
 
 
-def get_artist_top_tracks(artist_id, headers):
-    url = f'{base_url}artists/{artist_id}/top-tracks?country=IE'
-    r = requests.get(url, headers=headers)
-    tracks = r.json()['tracks']
-    return tracks
-
-
 def track_exists(track, existing_tracks):
     for existing_track in existing_tracks:
         if track['id'] == existing_track['id'] or track['name'] == existing_track['name']:
             return True
     return False
-
-
-def create_new_playlist(playlist_name, user_id, track_ids, headers):
-    headers['Content-Type'] = 'application/json'
-    url = f'{base_url}users/{user_id}/playlists'
-    body = json.dumps({'name': f'{playlist_name} - Shuffed'})
-    r = requests.post(url, data=body, headers=headers)
-    return r.json()
-
-
-def add_tracks_to_playlist(playlist_id, tracks, headers):
-    url = f'{base_url}playlists/{playlist_id}/tracks'
-    n = 100
-    tracks_split = [tracks[i:i+n] for i in range(0, len(tracks), n)]
-    # Can only add 100 tracks per request - multiple requests may be needed
-    for tracks_chunk in tracks_split:
-        body = json.dumps({'uris': tracks_chunk})
-        r = requests.post(url, data=body, headers=headers)
-    return r.text
