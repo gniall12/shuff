d29275850e307403f47693cc69efbc334f3966e8
Niall Gallagher <niall.gallagher@ucdconnect.ie>
2019-11-21 18:32:18 +0000

    Fixed error in multiline import (needed parentheses)

--- app.py
+++ app.py
@@ -5,8 +5,10 @@ import urllib
 import random
 import string
 
-from config import auth_base_url, token_base_url, client_id, client_secret, redirect_uri, 
-scope, response_type, secret_key, frontend_url, client_id_b64
+from config import (
+    auth_base_url, token_base_url, client_id, client_secret, redirect_uri,
+    scope, response_type, secret_key, frontend_url, client_id_b64
+)
 
 from shuffler import shuffle
 
