d21b77cee80848d6c3305b734e0eb4a30f43f762
Niall Gallagher <niall.gallagher@ucdconnect.ie>
2020-01-26 20:06:46 +0000

    Enforced get and post methods and checked json for shuffle playlist endpoint

--- app/main/app.py
+++ app/main/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, url_for, request, session
+from flask import Flask, redirect, url_for, request, session, abort, Response
 from flask_cors import CORS
 from config import secret_key, frontend_url
 from shuffler import shuffle
@@ -12,12 +12,12 @@ app.config['SECRET_KEY'] = secret_key
 logging.basicConfig(level=os.environ.get("LOGLEVEL", "INFO"))
 
 
-@app.route('/login')
+@app.route('/login', methods=['GET'])
 def login_redirect():
     return spotify.authorize(callback=url_for('handle_token', _external=True))
 
 
-@app.route('/callback')
+@app.route('/callback', methods=['GET'])
 def handle_token():
     resp = spotify.authorized_response()
     if resp is None or resp.get('access_token') is None:
@@ -27,13 +27,15 @@ def handle_token():
     return redirect(frontend_url)
 
 
-@app.route('/playlists')
+@app.route('/playlists', methods=['GET'])
 def playlists():
     return get_user_playlists()
 
 
 @app.route('/shuffle', methods=['POST'])
 def shuffle_playlist():
+    if('playlist' not in request.json):
+        abort(Response('Request missing playlist id', 400))
     playlist = request.json['playlist']
     new_playlist = shuffle(playlist)
     return new_playlist
