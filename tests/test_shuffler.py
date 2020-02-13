cae6def24ffcb4dacd25f7711569915adfff922b
Niall Gallagher <niallgallagher@Denise-Gallaghers-iMac.local>
2020-02-13 21:12:25 +0000

    Added unit test for get_new_track_ids method, mocking the call to spotify api

--- tests/test_shuffler.py
+++ tests/test_shuffler.py
@@ -1,25 +1,38 @@
+from unittest.mock import patch
+
 from app import shuffler
 
 tracks = [
     {
-        "id": 1,
+        "id": "1",
         "album": "Hold On",
         "artists": [{"name": "Little Dragon", "id": "1"}],
         "name": "Hold On",
+        "uri": "1",
     },
     {
-        "id": 2,
+        "id": "2",
         "album": "Circles",
         "artists": [{"name": "Mac Miller", "id": "2"}],
         "name": "Blue World",
+        "uri": "2",
     },
     {
-        "id": 3,
+        "id": "3",
         "album": "Baby",
         "artists": [{"name": "Four Tet", "id": "3"}],
         "name": "Baby",
+        "uri": "3",
     },
 ]
+
+test_track = {
+    "id": "5",
+    "album": "Test",
+    "artists": [{"name": "Test", "id": None}],
+    "name": "Test",
+    "uri": "5",
+}
 
 
 def test_get_old_artists_ids():
@@ -27,20 +40,21 @@ def test_get_old_artists_ids():
 
 
 def test_get_old_artists_ids_with_nones():
-    tracks.append(
-        {
-            "id": "5",
-            "album": "Test",
-            "artists": [{"name": "Test", "id": None}],
-            "name": "Test",
-        }
-    )
-    assert ["1", "2", "3"] == shuffler.get_old_artists_ids(tracks)
+    new_tracks = tracks.copy()
+    new_tracks.append(test_track)
+    assert ["1", "2", "3"] == shuffler.get_old_artists_ids(new_tracks)
+
+
+@patch("app.shuffler.get_artist_top_tracks")
+def test_get_new_track_ids(test_patch):
+    test_patch.return_value = [test_track]
+    x = shuffler.get_new_track_ids([1], tracks)
+    assert ["5"] == x
 
 
 def test_track_exists_by_id():
     track = {
-        "id": 1,
+        "id": "1",
         "album": "You and I",
         "artists": [{"name": "Caribou", "id": "4"}],
         "name": "You and I",
@@ -50,7 +64,7 @@ def test_track_exists_by_id():
 
 def test_track_exists_by_name():
     track = {
-        "id": 4,
+        "id": "4",
         "album": "Warpaint",
         "artists": [{"name": "Warpaint", "id": "4"}],
         "name": "Baby",
@@ -60,7 +74,7 @@ def test_track_exists_by_name():
 
 def test_track_doesnt_exist():
     track = {
-        "id": 5,
+        "id": "5",
         "album": "Supernatural",
         "artists": [{"name": "Floorplan", "id": "4"}],
         "name": "Oasis",
