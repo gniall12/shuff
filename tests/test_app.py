5cfbe1b42b979807bbef3f4dc2aec7d4bdb52730
Niall Gallagher <niallgallagher@Denise-Gallaghers-iMac.local>
2020-03-24 21:31:15 +0000

    Updated unit tests to match change to serving templates

--- tests/test_app.py
+++ tests/test_app.py
@@ -11,10 +11,35 @@ def test_client():
     return app.app.test_client()
 
 
+@patch("app.app.get_user_playlists")
+def test_home(get_user_playlists_patch, test_client):
+    with patch("app.app.session", {"access_token": "123"}) as session:
+        response = test_client.get("/")
+        assert get_user_playlists_patch.called
+
+
+@patch("app.app.url_for")
+@patch("app.app.redirect")
+def test_home_not_logged_in(redirect_patch, url_for_patch, test_client):
+    with patch("app.app.session", {}) as session:
+        response = test_client.get("/")
+        assert redirect_patch.called
+        assert url_for_patch.called
+
+
+@patch("app.app.url_for")
+@patch("app.app.redirect")
+def test_home_playlists_exception(redirect_patch, url_for_patch, test_client):
+    with patch("app.app.session", {"access_token": "123"}) as session:
+        test_client.get("/")
+        assert redirect_patch.called
+        assert url_for_patch.called
+
+
 @patch("app.app.url_for")
 @patch("app.app.spotify.authorize")
 def test_login_redirect(authorize_patch, url_for_patch, test_client):
-    response = test_client.get("/login")
+    test_client.get("/login-redirect")
     assert authorize_patch.called
 
 
@@ -41,22 +66,17 @@ def test_handle_token_no_response(
         assert "access_token" not in session
 
 
-@patch("app.app.get_user_playlists")
-def test_playlists(get_user_playlists_patch, test_client):
-    response = test_client.get("/playlists")
-    assert get_user_playlists_patch.called
-
 
 @patch("app.app.shuffle")
 def test_shuffle(shuffle_patch, test_client):
     playlist = {"name": "p1", "id": "1"}
     shuffle_patch.return_value = playlist
-    response = test_client.post("/shuffle", json={"playlist": "123"})
+    response = test_client.get("/shuffle?id=123&name=123")
     assert playlist == json.loads(response.data)
 
 
 def test_shuffle_bad_request(test_client):
-    response = test_client.post("/shuffle", json={})
+    response = test_client.get("/shuffle")
     assert 400 == response.status_code
 
 
@@ -64,4 +84,3 @@ def test_logout(test_client):
     with patch("app.app.session", {"access_token": "123"}) as session:
         response = test_client.get("/logout")
         assert "access_token" not in session
-        assert {"status": "Logged out"} == json.loads(response.data)
